df553ee5c012c61bb20d8565df0825961d98cffd
chiba <chiba@in-g.jp>
2022-09-20 01:02:08 +0900

    PageTitleコンポーネントの作成

---
+++ components/common/PageTitle.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+export const PageTitle = ({ children }: { children: ReactNode }) => {
+  return (
+    <Text fontWeight={"bold"} fontSize={20} textColor={"teal.500"}>
+      {children}
+    </Text>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { NavBar } from "../components/common/NavBar";
+import { Container } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <NavBar />
-      <Component {...pageProps} />
+      <Container maxW="container.lg" color="gray.700" py={30}>
+        <Component {...pageProps} />
+      </Container>
     </ChakraProvider>
   );
 }
