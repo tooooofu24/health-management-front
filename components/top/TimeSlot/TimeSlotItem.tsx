ccf6fdbcbd8a71f9a43ab4199a64238f68e80483
chiba <chiba@in-g.jp>
2022-09-28 04:30:48 +0900

    時間割のメニューを追加

--- components/top/TimeSlot/TimeSlotItem.tsx
+++ components/top/TimeSlot/TimeSlotItem.tsx
@@ -2,21 +2,16 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 import { DotsThree, Student, UsersThree } from "phosphor-react";
 import { Card } from "../../common/Card";
 import Router from "next/router";
+import { TimeSplotButton } from "./TimeSlotButton";
 
 export const TimeSlotItem = () => {
   return (
-    <Card
-      _hover={{ bg: "gray.50" }}
-      px="20px"
-      onClick={() => Router.push("/create/1")}
-    >
+    <Card width={160} onClick={() => Router.push("/create/1")}>
       <Flex justifyContent="space-between" alignItems="center">
         <Text fontWeight="bold" fontSize="17px">
           1年1組
         </Text>
-        <Box color="gray.500">
-          <DotsThree weight="bold" size={20} />
-        </Box>
+        <TimeSplotButton />
       </Flex>
       <Flex width="full" color="gray.500" gap="5px" pt="5px">
         <Flex alignItems="center">
