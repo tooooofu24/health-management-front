072c1f150c367f332a8caaa3e033a98f99770581
tooooofu24 <toya24xxx@i.softbank.jp>
2022-10-05 18:47:11 +0900

    クラス一覧UIの修正

--- components/classroom/index/ClassroomList.tsx
+++ components/classroom/index/ClassroomList.tsx
@@ -1,48 +1,63 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
-import { ClassCard } from "../../common/ClassCard";
-
+import {
+  Flex,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Badge,
+  Button,
+  Tag,
+} from "@chakra-ui/react";
+import { Pencil, Trash, Plus } from "phosphor-react";
 export const ClassroomList = () => {
   return (
-    <Flex flexDirection="column" gap="30px" p="20px">
-      <Flex flexDirection="column" gap="10px">
-        <Text fontWeight="bold" color="teal.500">
-          1年生
-        </Text>
-        <Flex flexWrap="wrap" gap="20px">
-          <ClassCard />
-          <ClassCard />
-          <ClassCard />
-          <ClassCard />
-          <ClassCard />
-          <ClassCard />
-        </Flex>
+    <TableContainer>
+      <Flex justifyContent="end" px="24px">
+        <Button leftIcon={<Plus />}>クラス新規登録</Button>
       </Flex>
-      <Flex flexDirection="column" gap="10px">
-        <Text fontWeight="bold" color="teal.500">
-          2年生
-        </Text>
-        <Flex flexWrap="wrap" gap="20px">
-          <ClassCard />
-          <ClassCard />
-          <ClassCard />
-          <ClassCard />
-          <ClassCard />
-          <ClassCard />
-        </Flex>
-      </Flex>
-      <Flex flexDirection="column" gap="10px">
-        <Text fontWeight="bold" color="teal.500">
-          3年生
-        </Text>
-        <Flex flexWrap="wrap" gap="20px">
-          <ClassCard />
-          <ClassCard />
-          <ClassCard />
-          <ClassCard />
-          <ClassCard />
-          <ClassCard />
-        </Flex>
-      </Flex>
-    </Flex>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th color="teal.500">学年・クラス</Th>
+            <Th color="teal.500">人数</Th>
+            <Th color="teal.500">担任</Th>
+            <Th color="teal.500">授業</Th>
+            <Th color="teal.500"></Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {[...Array(5)].map(() => {
+            return (
+              <Tr>
+                <Td>1年1組</Td>
+                <Td>33人</Td>
+                <Td>石田京楓</Td>
+                <Td>
+                  <Flex gap="10px">
+                    <Tag>音楽</Tag>
+                    <Tag>道徳</Tag>
+                  </Flex>
+                </Td>
+                <Td width="30%">
+                  <Flex gap="20px" justifyContent="end">
+                    <Button colorScheme="gray" leftIcon={<Pencil size={20} />}>
+                      編集する
+                    </Button>
+                    <Button colorScheme="red" leftIcon={<Trash size={20} />}>
+                      削除する
+                    </Button>
+                  </Flex>
+                </Td>
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </TableContainer>
   );
 };
