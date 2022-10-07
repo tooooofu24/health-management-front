84d387cb5938f509ede421f5ceb47ed764b4b39f
tooooofu24 <toya24xxx@i.softbank.jp>
2022-10-07 15:21:04 +0900

    時間割のUI微調整

--- components/top/TimeSlot/EmptyTimeSlotItem.tsx
+++ components/top/TimeSlot/EmptyTimeSlotItem.tsx
@@ -5,7 +5,7 @@ export const EmptyTimeSlotItem = () => {
   return (
     <Flex
       width="full"
-      height="230px"
+      height="190px"
       justifyContent="center"
       alignItems="center"
       _hover={{ button: { color: "gray.400" } }}
--- components/top/TimeSlot/TimeSlotItem.tsx
+++ components/top/TimeSlot/TimeSlotItem.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   ButtonGroup,
   Flex,
@@ -7,37 +8,41 @@ import {
   Tag,
   Text,
 } from "@chakra-ui/react";
+import Link from "next/link";
 import Router from "next/router";
-import { DotsThree, Flask } from "phosphor-react";
+import { DotsThree, Flask, X } from "phosphor-react";
 import { Card } from "../../common/Card";
 import { TimeSlotItemButton } from "./TimeSlotItemButton";
 
 export const TimeSlotItem = () => {
   return (
-    <Card width="full" height="230px">
-      <Flex flexDirection="column" gap="15px" height="full">
-        <Square flex="1">
-          <Flex color="teal.500" alignItems="center" justifyContent="center">
-            <Flask size={100} />
+    <Link href="/attendances/create">
+      <a style={{ width: "100%" }}>
+        <Card height="190px" position="relative">
+          <Box position="absolute" right="0" top="0" p="1px">
+            <IconButton
+              aria-label="削除"
+              icon={<X size={20} />}
+              variant="ghost"
+            />
+          </Box>
+          <Flex flexDirection="column" gap="15px" height="full">
+            <Square flex="1">
+              <Flex
+                color="teal.500"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Flask size={100} />
+              </Flex>
+            </Square>
+            <Flex alignItems="center" gap="10px">
+              <Text fontWeight="bold">1年3組</Text>
+              <Tag>理科</Tag>
+            </Flex>
           </Flex>
-        </Square>
-        <Flex alignItems="center" gap="10px">
-          <Text fontWeight="bold">1年3組</Text>
-          <Tag>理科</Tag>
-        </Flex>
-        <Flex justifyContent="end" gap="5px">
-          <ButtonGroup size="sm" isAttached>
-            <Button
-              onClick={() => {
-                Router.push("/attendances/create");
-              }}
-            >
-              成績登録
-            </Button>
-            <TimeSlotItemButton />
-          </ButtonGroup>
-        </Flex>
-      </Flex>
-    </Card>
+        </Card>
+      </a>
+    </Link>
   );
 };
