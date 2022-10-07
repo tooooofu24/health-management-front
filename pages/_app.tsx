9180d2a9c0e0e9339bb3ec99524b0259885aaa55
tooooofu24 <toya24xxx@i.softbank.jp>
2022-10-07 18:28:55 +0900

    ssl暫定対応

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,31 @@ import type { AppProps } from "next/app";
 import { Box, ChakraProvider, Flex } from "@chakra-ui/react";
 import { SideBar } from "../components/common/SideBar";
 import { CustomTheme } from "../chakra/CustomTheme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={CustomTheme}>
-      {/* <NavBar /> */}
-      <SideBar />
-      <Box pl="200px" pb="50px">
-        <Box p="30px">
-          <Component {...pageProps} />
+    <>
+      <Head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
+      <ChakraProvider theme={CustomTheme}>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+        {/* <NavBar /> */}
+        <SideBar />
+        <Box pl="200px" pb="50px">
+          <Box p="30px">
+            <Component {...pageProps} />
+          </Box>
         </Box>
-      </Box>
-    </ChakraProvider>
+      </ChakraProvider>
+    </>
   );
 }
 
