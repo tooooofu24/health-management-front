df7f1127399948d04acd2a7df4e623ec9ad9eb87
tooooofu24 <toya24xxx@i.softbank.jp>
2022-10-09 16:41:45 +0900

    headの整理

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,22 +3,11 @@ import type { AppProps } from "next/app";
 import { Box, ChakraProvider, Flex } from "@chakra-ui/react";
 import { SideBar } from "../components/common/SideBar";
 import { CustomTheme } from "../chakra/CustomTheme";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
-      </Head>
       <ChakraProvider theme={CustomTheme}>
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
         {/* <NavBar /> */}
         <SideBar />
         <Box pl="200px" pb="50px">
