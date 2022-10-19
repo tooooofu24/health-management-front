efa3b375980a0cea5aa041d0b388ab1af19bff8d
tooooofu24 <toya24xxx@i.softbank.jp>
2022-10-19 13:56:09 +0900

    教科アイコンの追加

--- components/common/SubjectIcon.tsx
+++ components/common/SubjectIcon.tsx
@@ -1,9 +1,16 @@
 import {
+  CookingPot,
+  DesktopTower,
   Flask,
+  Globe,
   GlobeStand,
+  Heart,
   IconProps,
   MathOperations,
+  MusicNotes,
   Notebook,
+  PaintBrush,
+  PersonSimpleRun,
   Question,
   Translate,
 } from "phosphor-react";
@@ -17,5 +24,12 @@ export const SubjectIcon = ({ subjectName, ...props }: props) => {
   if (subjectName === "社会") return <GlobeStand {...props} />;
   if (subjectName === "理科") return <Flask {...props} />;
   if (subjectName === "英語") return <Translate {...props} />;
+  if (subjectName === "保健体育") return <PersonSimpleRun {...props} />;
+  if (subjectName === "音楽") return <MusicNotes {...props} />;
+  if (subjectName === "家庭") return <CookingPot {...props} />;
+  if (subjectName === "美術") return <PaintBrush {...props} />;
+  if (subjectName === "技術") return <DesktopTower {...props} />;
+  if (subjectName === "道徳") return <Heart {...props} />;
+  if (subjectName === "総合") return <Globe {...props} />;
   return <Question {...props} />;
 };
