19c62043bfea864bcf371ddffb2c22d5ab4ccc09
tooooofu24 <toya24xxx@i.softbank.jp>
2022-10-21 22:03:25 +0900

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,28 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import { Box, ChakraProvider, Flex } from "@chakra-ui/react";
-import { SideBar } from "../components/common/SideBar";
+import { Box, ChakraProvider, Flex, useToast } from "@chakra-ui/react";
 import { CustomTheme } from "../chakra/CustomTheme";
-import { NavBar } from "../components/common/NavBar";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useRouter } from "next/router";
 import { app } from "../utils/firebase";
 import { refreshBearerToken } from "../utils/bearer";
+import { logout } from "../utils/auth";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const auth = getAuth(app);
   refreshBearerToken();
-  onAuthStateChanged(auth, (user) => {
+  onAuthStateChanged(auth, async (user) => {
     if (!user && router.pathname != "/login" && router.route != "/404") {
+      await logout();
       router.push("/login");
     }
   });
 
   return (
-    <>
-      <ChakraProvider theme={CustomTheme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </>
+    <ChakraProvider theme={CustomTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
 
