bc90d88a77e3ef4d95df1cdf41d31370392ed1b5
tooooofu24 <toya24xxx@i.softbank.jp>
2022-11-05 17:38:16 +0900

    fix

--- components/users/Index.tsx
+++ components/users/Index.tsx
@@ -163,7 +163,7 @@ const InvitationRow: FC<InvitationRowProps> = ({ invitation }) => {
       </Td>
       <Td>{invitation.email}</Td>
       <Td>{invitation.createdBy.name}</Td>
-      <Td>{invitation.createdAt}</Td>
+      <Td>{formatDate(invitation.createdAt)}</Td>
       <Td>
         <CancelInvitationButtton invitation={invitation} />
       </Td>
