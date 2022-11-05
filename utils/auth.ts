c97708b763a9653a63d1a7f77c09a8344f4f73db
tooooofu24 <toya24xxx@i.softbank.jp>
2022-11-05 19:30:44 +0900

    firebaseのエラーハンドリング

--- utils/auth.ts
+++ utils/auth.ts
@@ -6,17 +6,25 @@ import {
 } from "firebase/auth";
 import { app } from "./firebase";
 import { useCheckUser } from "../hooks/Auth";
+import { FirebaseError } from "firebase/app";
 
 export const login = async () => {
   const provider = new GoogleAuthProvider();
   const auth = getAuth(app);
   const { checkUser } = useCheckUser();
-  await signInWithPopup(auth, provider).then(async (results) => {
-    await checkUser().catch(async (e: any) => {
-      await logout();
-      throw e;
+  await signInWithPopup(auth, provider)
+    .catch((e: Error) => {
+      if (e instanceof FirebaseError) {
+        throw new Error("システムのエラーが発生しました");
+      }
+      throw new Error("不明なエラーです");
+    })
+    .then(async (results) => {
+      await checkUser().catch(async (e: any) => {
+        await logout();
+        throw e;
+      });
     });
-  });
 };
 
 export const logout = async () => {
